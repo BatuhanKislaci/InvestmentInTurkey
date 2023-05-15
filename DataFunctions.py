b672cb4631eca547cc8bbd61d88e143c02ddb214
BatuhanKislaci <72382902+BatuhanKislaci@users.noreply.github.com>
2023-05-15 15:49:02 +0300

    Update DataFunctions.py

--- DataFunctions.py
+++ DataFunctions.py
@@ -6,15 +6,18 @@ def TurkeyGeneral(importData,exportData):
     years = importData["Year"]
 
     oldData = []
+    lineData = []
     for i in range(0,10):
         oldData.append([imports[i],exports[i]])
+        lineData.append(imports[i] / exports[i])
+
     
     
 
     generalDF = pd.DataFrame(oldData,columns=["Import","Export"],index=years)
+    lineDF = pd.DataFrame(lineData,columns=["Import/Export"] ,index = years)
     print("In Turkey's economy imports are higher than exports so investing in one of the imported goods will be beneficial.")
     
     generalDF.plot.line()
+    lineDF.plot.line()
     plt.show()
-
-
